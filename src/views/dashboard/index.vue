f3fbef4a99979ab80aff2201ab7051ab8e682085
KeanuEcho <c2003402@163.com>
2023-02-15 16:59:42 +0800

    提交一些更改

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="dashboard-container">
     <div class="dashboard-text">欢迎您： {{ name }}</div>
+    <br>
+    <div style="font-size: 25px">嗨嗐嗨！交作业辣！</div>
+    <br>
+    <div style="font-size: 20px">点击侧边栏中的【提交作业】选项，选择一门课程，即可开始提交您的心血！</div>
   </div>
 </template>
 
